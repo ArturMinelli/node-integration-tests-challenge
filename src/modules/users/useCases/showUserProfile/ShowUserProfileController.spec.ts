b8b3732dbb824f9aa0cd23a18e95d885b6f4acaa
Artur Minelli Peixoto <arturminelli@gmail.com>
2023-01-21 08:46:01 -0300

    Creating ShowUserProfile Controller

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.spec.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.spec.ts
@@ -3,9 +3,10 @@ import { app } from '../../../../app'
 
 import createConnection from '../../../../database'
 import { Connection } from 'typeorm'
-import { profile } from 'console'
 
 let connection: Connection
+
+let fakeUserToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1N2QzODc0NC02ZTQzLTQ1ZmMtODMxNS05ZGY5ODE4ODc3Y2YiLCJ1c2VyIjp7ImlkIjoiNTdkMzg3NDQtNmU0My00NWZjLTgzMTUtOWRmOTgxODg3N2NmIiwibmFtZSI6Ik5vbiBFeGlzdGluZyBVc2VyIiwiZW1haWwiOiJub24uZXhpc3RpbmcudXNlckBnbWFpbC5jb20ifSwiaWF0IjoiIn0.uCUcL26yWiyc0RMiwpWEuyfoMz6pwS6i7gbCWYP7i2Q"
 
 describe("Create Category Controller", () => {
 
@@ -19,7 +20,7 @@ describe("Create Category Controller", () => {
     await connection.close()
   })
 
-  it('should be able to create a new user', async () => {
+  it('should be able to authenticate an user', async () => {
     const name = 'Iron Man'
     const email = 'iron@man.com'
     const password = 'fridayjarvis'
@@ -50,6 +51,50 @@ describe("Create Category Controller", () => {
       name,
       email
     })
+  })
+
+  it("should not be able to get an user's profile if authentication toke is invalid", async () => {
+    const name = 'Iron Man'
+    const email = 'iron@man.com'
+    const password = 'fridayjarvis'
+
+    await request(app).post('/api/v1/users')
+      .send({
+        name,
+        email,
+        password
+      })
+
+    const token = 'token_that_will_not_work'
+
+    const profileResponse = await request(app).get('/api/v1/profile')
+      .set('Authorization', `Bearer ${token}`)
+
+    expect(profileResponse.statusCode).toBe(401)
+  })
+
+  it("should not be able to get an user's profile if authentication toke is missing", async () => {
+    const name = 'Iron Man'
+    const email = 'iron@man.com'
+    const password = 'fridayjarvis'
+
+    await request(app).post('/api/v1/users')
+      .send({
+        name,
+        email,
+        password
+      })
+
+    const profileResponse = await request(app).get('/api/v1/profile')
+
+    expect(profileResponse.statusCode).toBe(401)
+  })
+
+  it("should not be able to get an user's profile if user does not exist", async () => {
+    const profileResponse = await request(app).get('/api/v1/profile')
+      .set('Authorization', `Bearer ${fakeUserToken}`)
+
+    expect(profileResponse.statusCode).toBe(404)
   })
 
 })
